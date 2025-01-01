01fb739854100a5340e3839b87d479da187f834f
yangyang <charlesmaxwellyoung@gmail.com>
2025-01-02 00:25:28 +0800

    feat: 优化字体功能

--- scripts/optimizationFont.js
+++ scripts/optimizationFont.js
@@ -14,7 +14,7 @@ const textSubset = frontMatters
 console.log("markdownFiles", textSubset);
 const fontmin = new Fontmin()
   // 字体的源文件
-  .src("public/fonts/SmileySans-Oblique.otf.woff2")
+  .src("public/fonts/SmileySans-Oblique.ttf")
   .use(
     Fontmin.glyph({
       text: textSubset,
@@ -22,7 +22,7 @@ const fontmin = new Fontmin()
     })
   )
   // 文件生成成后放在网站的资源目录中，Rollup在后面会复制到dist目录里去
-  .dest("public/fonts");
+  .dest("public/fonts/");
 // 开始精简字体
 fontmin.run((err, files) => {
   console.log("files", files);
